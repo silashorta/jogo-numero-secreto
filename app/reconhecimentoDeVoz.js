2c50ff3e4dbbeae853d1b2e2ffbde80aa432d566
silashorta <92862835+silashorta@users.noreply.github.com>
2023-11-28 02:24:19 -0300

    Update reconhecimentoDeVoz.js

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,67 +1,8 @@
-/* const elementoChute = document.querySelector('#chute')
-
-
-// CONFIGURANDO E INICIANDO O SPEECHRECOGNITION
-
-const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
-
-const recognition = new SpeechRecognition();
-recognition.lang = 'pt-BR'
-
-
-
-const mic = document.querySelector('.buttonMic')
-mic.addEventListener('mousedown', (e) => {
-        recognition.start(e); 
-})
-mic.addEventListener('mouseup', (e) => {
-    recognition.stop(e);
-})
-mic.addEventListener('touchstart', (e) => {
-    e.preventDefault()
-        recognition.start(e); 
-})
-mic.addEventListener('touchend', (e) => {
-    recognition.stop(e);
-})
-
-// CAPTURAR A FALA
-
-recognition.addEventListener('result', onSpeak)
-
-function onSpeak(e){
-    chute = e.results[0][0].transcript
-    exibeChuteNaTela(chute)
-    VerificaValor(chute)
-}
-
-function exibeChuteNaTela(chute) {
-    //console.log(chute)
-    elementoChute.innerHTML = 
-    `<div class="mensagem">Você disse:</div>
-    <div class="box">${chute}</div>`
-    listaChute(chute)
-}
-
-const listaArray = []
-function listaChute(chute) {
-    listaArray.unshift(chute)
-    
-    const lista = document.querySelector('.listaChute')
-    lista.innerHTML = `Suas tentativas: ${listaArray.join(", ")}`
-    
-    //console.log(listaArray)
-}
-
-// REATIVANDO O MICROFONE
-
-//recognition.addEventListener('end', () => recognition.start()) */
-
 
 const elementoChute = document.querySelector('#chute');
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
-/* recognition.lang = 'pt-BR'; */
+recognition.lang = 'pt-BR'; 
 const mic = document.querySelector('.buttonMic');
 
 let isRecognitionStarted = false;
