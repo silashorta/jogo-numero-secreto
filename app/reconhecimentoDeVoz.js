a9491b5c7baa12291d0bcf5946cf6a078eac6bef
silashorta <92862835+silashorta@users.noreply.github.com>
2023-11-30 17:01:44 -0300

    Update reconhecimentoDeVoz.js

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -1,4 +1,4 @@
-
+/* 
 const elementoChute = document.querySelector('#chute');
 const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
@@ -53,6 +53,67 @@ function exibeChuteNaTela(chute) {
         `<div class="mensagem">Você disse:</div>
         <div class="box">${chute}</div>`;
     listaChute(chute);
+}
+
+const listaArray = [];
+
+function listaChute(chute) {
+    listaArray.unshift(chute);
+    const lista = document.querySelector('.listaChute');
+    lista.innerHTML = `Suas tentativas: ${listaArray.join(", ")}`;
+}
+ */
+
+const elementoChute = document.querySelector('#chute');
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+recognition.lang = 'pt-BR'; 
+const mic = document.querySelector('.buttonMic');
+
+let isRecognitionStarted = false;
+
+mic.addEventListener('touchstart', startRecognition);
+mic.addEventListener('mousedown', startRecognition);
+
+function startRecognition(e) {
+    e.preventDefault();
+
+    if (!isRecognitionStarted) {
+        console.log('Starting recognition...');
+        recognition.start();
+        isRecognitionStarted = true;
+
+        recognition.addEventListener('end', () => {
+            console.log('Recognition ended.');
+            isRecognitionStarted = false;
+        });
+
+        recognition.addEventListener('result', onSpeak);
+
+        // Adicione um ouvinte para o evento touchend na janela para garantir que o reconhecimento não seja interrompido ao tocar na tela
+        window.addEventListener('touchend', windowTouchEndHandler, { once: true });
+        window.addEventListener('mouseup', windowTouchEndHandler, { once: true });
+    }
+}
+
+function windowTouchEndHandler() {
+    console.log('Touch end on window.');
+    if (isRecognitionStarted) {
+        recognition.stop();
+    }
+}
+
+function onSpeak(e) {
+    const chute = e.results[0][0].transcript;
+    console.log('Speech recognized:', chute);
+    exibeChuteNaTela(chute);
+    listaChute(chute);
+}
+
+function exibeChuteNaTela(chute) {
+    elementoChute.innerHTML =
+        `<div class="mensagem">Você disse:</div>
+        <div class="box">${chute}</div>`;
 }
 
 const listaArray = [];
